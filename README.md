21105d997ad6fc6f2d24c4f2108432514b18c47f
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2024-02-07 00:56:41 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-jzwe6z
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/GGiunashvili/js-jzwe6z)
---
+++ index.js
@@ -0,0 +1,39 @@
+//1
+console.log('hello world');
+
+//2
+let x;
+x = 5;
+console.log(5);
+
+//3
+console.log(5 + 4);
+
+//4
+console.log(5 - 4);
+
+//5
+console.log(5 * 4);
+
+//6
+console.log(5 / 4);
+
+//7
+console.log(5 % 4);
+
+//8
+let y;
+y = 'string';
+console.log(typeof y);
+
+//9
+let a = 'stringA';
+let b = ' stringB';
+console.log(a + b);
+console.log('stringA' + ' stringB');
+
+//10
+console.log(a.toUpperCase());
+
+//11
+console.log(b.toLocaleLowerCase());
